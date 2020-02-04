15a4c1f245f881c0b89c5bd2c9886db167fb59de
Jorge Menezes <jorgefernandosaraiva@gmail.com>
2020-02-04 17:18:17 -0400

    added first maxent calculation

--- code/maxenter.r
+++ code/maxenter.r
@@ -17,10 +17,12 @@
 maxenter <- function(data, obsdir, studydir, evalfile, outfile, nc) {
 
 # Read data, and select some locations to be the train and test dataset.
-presences <- st_read(data) %>% mutate(istest = sample(0:1,nrow(mov.track), prob =c(0.8,0.2), replace=T))
+presences <- st_read(data)
+presences$test <- sample(0:1,nrow(presences), prob =c(0.8,0.2), replace=T)
+    
 
 # Read observed stack
-obsstack  <- stack(list.files(tempdir,pattern="tif$",full.names=T))
+obsstack  <- stack(list.files(obsdir,pattern="tif$",full.names=T))
 
 # Since there are a lot of NA space in the raster stack I feel I should make sure random
 # pseudo-absences fall in a valid area. For that I will use random points with a mask
@@ -30,22 +32,25 @@ mask <- subs(obsstack$landuse,data.frame(0,NA),subsWithNA=FALSE)
 absences      <- randomPoints(mask, 10000,p= st_coordinates(presences))
 absences.test <- sample(0:1,nrow(absences), prob =c(0.8,0.2), replace=T)
 
+
 # Run maxent model
-model <- maxent(obsstack, presences[mov.track$test==0,], absences = absences[absences.test==0,], factors="landuse")
+model <- maxent(obsstack, st_coordinates(presences[presences$test==0,]), absences = absences[absences.test==0,], factors="landuse")
 
 # Evaluate the model
-presence.testquali <- predict(model, as.data.frame(raster::extract(mapstack, presences[mov.track$test==1,]))  )
-absence.testquali  <- predict(model, as.data.frame(raster::extract(mapstack, absences[absences.test==1,]))  )
+presence.testquali <- predict(model, as.data.frame(raster::extract(obsstack, st_coordinates(presences[presences$test==1,])))  )
+absence.testquali  <- predict(model, as.data.frame(raster::extract(obsstack, absences[absences.test==1,]))  )
 auc.test <- evaluate(presence.testquali,absence.testquali)
 saveRDS(auc.test, file = evalfile)
 
 # Read study area mapstack
 studystack <-  stack(list.files(studydir,pattern="tif$",full.names=T))
 
+# FOR DEBUG:
+#studystack <-  crop(studystack, extent(500000,501000,-1250000,-1245000))
 
 # Use predict with clusterR to speed up process
 beginCluster(nc)
-clusterR(studystack, predict, args = list(model = maxent), filename = outfile )
+clusterR(studystack, predict, args = list(model = model), filename = outfile )
 endCluster()
 
 return(outfile)
